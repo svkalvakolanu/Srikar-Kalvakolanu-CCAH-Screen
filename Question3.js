c9a59d69468a977825bbb3f4af52c579f1a50bf3
svkalvakolanu <skalvakolanu@gmail.com>
2020-04-02 19:17:36 -0400

    small modifications

--- Question3.js
+++ Question3.js
@@ -1,6 +1,5 @@
-let copyText = document.querySelector("p")
-
-let currentURL = window.location.href
+const copyText = document.querySelector("p")
+const currentURL = window.location.href
 
 if(currentURL.split("=")[1] == "returning"){
     copyText.innerHTML = "Thanks for coming back to the website!"
